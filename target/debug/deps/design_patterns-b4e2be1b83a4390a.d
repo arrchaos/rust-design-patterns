2e6b57f0f638bc6ecacd8d9b22cd8701403bf3b7
Victor Zhibinov <victor@Victors-MacBook-Pro.local>
2022-11-07 07:25:23 +0200

    add gitignore

---
+++ target/debug/deps/design_patterns-b4e2be1b83a4390a.d
@@ -0,0 +1,5 @@
+/Users/victor/rust/design-patterns/target/debug/deps/design_patterns-b4e2be1b83a4390a.rmeta: src/main.rs
+
+/Users/victor/rust/design-patterns/target/debug/deps/design_patterns-b4e2be1b83a4390a.d: src/main.rs
+
+src/main.rs:
