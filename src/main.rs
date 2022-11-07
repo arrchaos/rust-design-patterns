6108cd1418da48e919b6a12f2667e4727b659410
Victor Zhibinov <victor@Victors-MacBook-Pro.local>
2022-11-07 07:32:14 +0200

    remove unused code from main

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,5 @@
 mod structural;
-use structural::{test, responsiblity_chain::*};
 
 fn main() {
-   let t1 = test::A1::new("Test print");
-   t1.print();
-
-
-
-
-//    let mut patient = Patient {
-//     name: "John".into(),
-//     ..Patient::default()
-//    };
-
-//    test4.execute(&mut patient);
-
-
-
-   //let t2 = responsiblity_chain::cor();
-   //t1.print();
+   println!("nothing")
 }
