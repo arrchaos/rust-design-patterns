9a54ef2a26ee4ead8e5b99dcb0e51e72902b679e
Victor Zhibinov <victor@Victors-MacBook-Pro.local>
2022-11-07 07:37:54 +0200

    fixed gitignore

--- target/debug/deps/design_patterns-b4e2be1b83a4390a.d
+++
@@ -1,5 +0,0 @@
-/Users/victor/rust/design-patterns/target/debug/deps/design_patterns-b4e2be1b83a4390a.rmeta: src/main.rs
-
-/Users/victor/rust/design-patterns/target/debug/deps/design_patterns-b4e2be1b83a4390a.d: src/main.rs
-
-src/main.rs:
--- target/debug/deps/design_patterns-fb3ca4adeb808d5c.d
+++
@@ -1,5 +0,0 @@
-/Users/victor/rust/design-patterns/target/debug/deps/design_patterns-fb3ca4adeb808d5c.rmeta: src/main.rs
-
-/Users/victor/rust/design-patterns/target/debug/deps/design_patterns-fb3ca4adeb808d5c.d: src/main.rs
-
-src/main.rs:
