f0a48f77de68c099bcfdfdc4eed376888742cbff
Victor Zhibinov <victor@Victors-MacBook-Pro.local>
2022-11-07 07:24:29 +0200

    add gitignore

--- target/debug/deps/design_patterns-fb3ca4adeb808d5c.d
+++ target/debug/deps/design_patterns-fb3ca4adeb808d5c.d
@@ -1,9 +1,8 @@
-/Users/victor/rust/design-patterns/target/debug/deps/design_patterns-fb3ca4adeb808d5c.rmeta: src/main.rs src/structural/mod.rs src/structural/test.rs src/structural/responsiblity_chain/mod.rs src/structural/responsiblity_chain/cor.rs
+/Users/victor/rust/design-patterns/target/debug/deps/design_patterns-fb3ca4adeb808d5c.rmeta: src/main.rs src/structural/mod.rs src/structural/responsiblity_chain/mod.rs src/structural/responsiblity_chain/cor.rs
 
-/Users/victor/rust/design-patterns/target/debug/deps/design_patterns-fb3ca4adeb808d5c.d: src/main.rs src/structural/mod.rs src/structural/test.rs src/structural/responsiblity_chain/mod.rs src/structural/responsiblity_chain/cor.rs
+/Users/victor/rust/design-patterns/target/debug/deps/design_patterns-fb3ca4adeb808d5c.d: src/main.rs src/structural/mod.rs src/structural/responsiblity_chain/mod.rs src/structural/responsiblity_chain/cor.rs
 
 src/main.rs:
 src/structural/mod.rs:
-src/structural/test.rs:
 src/structural/responsiblity_chain/mod.rs:
 src/structural/responsiblity_chain/cor.rs:
